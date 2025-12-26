76175c2938af43764f9b4e52d11a63fc67841425
Ваше Имя <.email@example.com>
2025-12-26 15:59:59 +1000

    добавить сервер хаб

--- CheckersServer/Program.cs
+++ CheckersServer/Program.cs
@@ -1,6 +1,23 @@
+using CheckersServer.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors();
+app.UseHttpsRedirection();
+app.MapHub<CheckersHub>("/checkersHub");
 
 app.Run();
